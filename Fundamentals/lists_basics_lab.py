a016e850156af94ca17693ad524d4809eba117c9
Daarkey <dimitrova_moni@yahoo.com>
2024-01-29 23:30:50 +0200

    Adding some lists_basics exercises

--- Fundamentals/lists_basics_lab.py
+++ Fundamentals/lists_basics_lab.py
@@ -17,21 +17,44 @@
 
 #2
 
-factor = int(input())
-count = int(input())
+# factor = int(input())
+# count = int(input())
 
-# my_list = [factor]
+# # my_list = [factor]
 
-# for i in range(factor, count + 1):
+# # for i in range(factor, count + 1):
+# #     new_num = i * factor
+# #     my_list.append(new_num)
+
+# # print(my_list)
+
+# my_list = []
+
+# for i in range(1, count + 1):
 #     new_num = i * factor
 #     my_list.append(new_num)
 
 # print(my_list)
 
-my_list = []
 
-for i in range(1, count + 1):
-    new_num = i * factor
-    my_list.append(new_num)
+#3
 
-print(my_list)
+# red_card = input()
+
+# list_of_red_cards = red_card.split()
+
+# print(f"Team A - {final_a_players}; Team B - {final_b_players}")
+
+# if team_a_player_count >= 7 and team_b_player_count >=7:
+
+
+#4
+
+int_string = input()
+count_beggars = int(input())
+splitted_string_list = int_string.split(", ")
+
+# for i in range(0, len(splitted_string_list) - 1):
+#     if i % 2 == 0:
+#         splitted_string_list[i] =
+print(len(splitted_string_list) / count_beggars)
