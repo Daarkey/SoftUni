524c2cfc015930e084835eb1e5028413d23f1065
Daarkey <dimitrova_moni@yahoo.com>
2024-02-05 23:09:35 +0200

    Updated functions_lab

---
+++ Fundamentals/functions_lab.py
@@ -0,0 +1,27 @@
+"""1. Smallest of Three Numbers"""
+
+# def smallest_num(num1, num2, num3):
+#     return min(num1, num2, num3)
+
+
+# number1, number2, number3 = int(input()), int(input()), int(input())
+
+# print(smallest_num(number1, number2, number3))
+
+
+"""2. Add and Subtract"""
+
+# def sum_numbers(num1, num2):
+#     return num1 + num2
+
+
+# def subtract(num1, num2, num3):
+#     return (num1 + num2) - num3
+
+
+# def add_and_subtract(num1, num2, num3):
+#     return subtract(num1, num2, num3)
+
+
+# number1, number2, number3 = int(input()), int(input()), int(input())
+# print(add_and_subtract(number1, number2, number3))
